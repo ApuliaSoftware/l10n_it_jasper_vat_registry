37520315b3ae6816c7b05106c6669d8cbd387ad9
Giuseppe <g.dalo@cgsoftware.it>
2016-11-25 13:04:54 +0100

    corretta la definizione del o2m

--- account.py
+++ account.py
@@ -31,7 +31,7 @@ class vat_registry(orm.Model):
         'reg_vat_code': fields.char('Codice Registro', size=10),
         'name': fields.char('Descrizione', size=30),
         'activity_code': fields.char('Codice Attivita', size=10),
-        'journal_ids': fields.one2many('vat.registry.journals', 'Lista Sezionali'),
+        'journal_ids': fields.one2many('vat.registry.journals', 'vat_registry_id',''Lista Sezionali'),
         'type': fields.selection(
                 (('Vendite', 'vendite'), ('Acquisti', 'acquisti'), ('Corrispettivi', 'corrispettivi')),
                 'Tipo Registro'),
